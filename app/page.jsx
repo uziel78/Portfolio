99f12b56927d5a0bee622f49956a6d7c3a7b9218
uziel78 <rune.husa78@gmail.com>
2023-05-03 13:51:13 +0200

    basic setup

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+//import Image from 'next/image';
+import styles from './page.module.scss';
+
+const HomePage = () => {
+  return (
+    <main className={styles.main}>
+      <div>Test...</div>
+    </main>
+  );
+};
+
+export default HomePage;
